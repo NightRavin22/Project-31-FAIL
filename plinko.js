30ea0c2f84a30ea44a2aca91f12a4ca87b0ac391
NightRavin22 <73198686+NightRavin22@users.noreply.github.com>
2020-12-10 18:50:20 -0500

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,31 @@
+class Plinko
+{
+	constructor(x,y)
+	{
+		var options={
+			isStatic:false,
+			restitution:1,
+			friction:0,
+			density:0.8
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.radius=10;
+		this.body=Bodies.circle(this.x, this.y, (this.radius)/2, options)
+		World.add(world, this.body);
+
+	}
+    display(){
+var pos=this.body.position;
+var angle=this.body.angle;
+push();
+translate(pos.x,pos.y);
+rotate(angle);
+fill("white");
+ellipseMode(CENTER);
+ellipse(0,0,this.radius,this.radius);
+pop();
+}
+}
+
